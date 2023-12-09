0124cebcdfab4f3129835262365776e394a83b61
yaongdaong <haeryang2@naver.com>
2023-12-09 23:04:13 +0900

    홍팍

---
+++ src/main/java/com/example/hongpark/HongparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hongpark/HongparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
