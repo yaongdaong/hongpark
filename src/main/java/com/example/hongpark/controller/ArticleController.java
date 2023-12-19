ef1a44d4d2645839f9cb6f89b3c2271173fe1e2c
yaongdaong <haeryang2@naver.com>
2023-12-19 21:21:04 +0900

    데이터조회

--- src/main/java/com/example/hongpark/controller/ArticleController.java
+++ src/main/java/com/example/hongpark/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.hongpark.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
 
 // 로깅 기능을 위한 어노테이션 추가
 @Slf4j
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController {
     @Autowired // 스프링부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
+    // DB에서 데이터를 가져오는 주체
     private ArticleRepository articleRepository;
 
     // URL 요청 접수
@@ -40,5 +46,19 @@ public class ArticleController {
         //System.out.println(saved.toString());
         // 실제 서버에서는 pringln()문으로 데이터를 검증하면 기록에 남지 않을뿐더러 서버의 성능에도 악영향을 끼침
         return "";
+    }
+
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
+    // @PathVariable은 URL요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져오는 어노테이션
+    public String show(@PathVariable Long id, Model model){ // 매개변수로 id 받아 오기
+        log.info("id = "+id); // id를 잘 받았는지 확인하는 로그 찍기
+        //1. id를 조회해 db에서 해당 데이터 가져오기
+        Optional<Article> articleEntity = articleRepository.findById(id);
+        //Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 가져온 데이터를 모델에 등록하기
+        // 데이터를 모델에 등록하는 이유는 MVC 패턴에 따라 조회한 데이터를 뷰페이지에서 사용하기 위해서임
+        model.addAttribute("article",articleEntity);
+        //3. 조회한 데이터를 사용자에게 보여 주기 위한 뷰 페이지 만들기
+        return "articles/show";
     }
 }
--- src/main/java/com/example/hongpark/dto/MemberForm.java
+++ src/main/java/com/example/hongpark/dto/MemberForm.java
@@ -4,6 +4,13 @@ import com.example.hongpark.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+//데이터 조회 과정
+//1. 사용자가 데이터를 조회해 달라고 웹 페이지에서 URL 요청을 보냄
+//2. 서버의 컨트롤러가 요청을 받아 해당 URL에서 찾으려는 데이터 정보를 리파지터리에 전달
+//3. 리파지터리는 정보를 가지로 DB에 데이터 조회를 요청
+//4. DB는 해당 데이터를 찾아 이를 엔티티로 반환
+//5. 반환된 엔티티는 모델을 통해 뷰 템플릿으로 전달
+//6. 최종적으로 결과 뷰 페이지가 완성돼 사용자의 화면에 출력됨
 @AllArgsConstructor
 @ToString
 public class MemberForm {
