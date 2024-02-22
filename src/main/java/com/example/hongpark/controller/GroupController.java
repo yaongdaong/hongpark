55b2c7f80774dd95ad4633e3889e9a9e5603977e
yaongdaong <haeryang2@naver.com>
2024-02-22 20:40:21 +0900

    댓글 생성

--- src/main/java/com/example/hongpark/controller/GroupController.java
+++ src/main/java/com/example/hongpark/controller/GroupController.java
@@ -1,23 +1,23 @@
-package com.example.hongpark.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@Controller
-public class GroupController {
-    @Autowired
-    private GroupService groupService;
-    @Autowired
-    private TeamService teamService;
-    @GetMapping("/groups/{groupId}")
-    public String show(@PathVariable Long groupId, Model model){
-        GroupDto groupDto = groupService.getGroup(groupId);
-        List<TeamDto> teamDtos = teamService.getTeamsOnGroup(groupId);
-        model.addAttribute("groupname",groupDto.getName());
-        model.addAttribute("teamDtoList",teamDtos);
-        return "groups/show";
-    }
-}
+//package com.example.hongpark.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//@Controller
+//public class GroupController {
+//    @Autowired
+//    private GroupService groupService;
+//    @Autowired
+//    private TeamService teamService;
+//    @GetMapping("/groups/{groupId}")
+//    public String show(@PathVariable Long groupId, Model model){
+//        GroupDto groupDto = groupService.getGroup(groupId);
+//        List<TeamDto> teamDtos = teamService.getTeamsOnGroup(groupId);
+//        model.addAttribute("groupname",groupDto.getName());
+//        model.addAttribute("teamDtoList",teamDtos);
+//        return "groups/show";
+//    }
+////}
